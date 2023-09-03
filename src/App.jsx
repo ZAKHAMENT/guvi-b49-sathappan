b8b588e0afa5ef8a088f5d1da26d9e5bc0335e85
SATHAPPAN <135624204+ZAKHAMENT@users.noreply.github.com>
2023-09-03 13:19:52 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,183 @@
-import React from 'react';
-import { CartProvider } from './CartContext';
-import Product from './Product';
-import Cart from './Cart';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
-const products = [
-  {
-    id: 1,
-    title: "iPhone 9",
-    description: "An apple mobile which is nothing like apple",
-    price: 549,
-    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+const API_URL = 'http://localhost:4000/posts';
+// const link = 'https://jsonplaceholder.typicode.com/users'
+const initialNewUser = {
+  name: '',
+  email: '',
+  username: '',
+  address: {
+    street: '',
+    suite: '',
+    city: '',
+    zipcode: '',
+    geo: {
+      lat: '',
+      lng: '',
+    },
   },
-
-  {
-    id: 1,
-    title: "iPhone X",
-    description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip",
-    price: 899,
-    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-  },
-
-  {
-    id: 1,
-    title: "Samsung Universe 9",
-    description: "Samsung's new variant which goes beyond Galaxy to the Universe",
-    price: 1249,
-    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-  },
-
-  {
-    id: 1,
-    title: "OPPOF19",
-    description: "OPPO F19 is officially announced on April 2021",
-    price: 280,
-    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-  },
-
-  {
-    id: 1,
-    title: "Huawei P30",
-    description: "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK",
-    price: 499,
-    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-  }
-  // Add more product data here
-];
-
-const App = () => {
-  return (
-    <CartProvider>
-      <div className="App">
-        {products.map(product => (
-          <Product
-            key={product.id}
-            id={product.id}
-            title={product.title}
-            description={product.description}
-            price={product.price}
-            thumbnail={product.thumbnail}
-          />
-        ))}
-        <Cart />
-      </div>
-    </CartProvider>
-  );
 };
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState(initialNewUser);
+  const [editUserId, setEditUserId] = useState(null);
+
+  useEffect(() => {
+    // Fetch users from the mock API
+    axios.get(API_URL)
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const handleAddUser = () => {
+    // Add a new user to the mock API
+    axios.post(API_URL, newUser)
+      .then((response) => {
+        setUsers([...users, response.data]);
+        setNewUser(initialNewUser);
+      })
+      .catch((error) => {
+        console.error('Error adding user:', error);
+      });
+  };
+
+  const handleEditUser = (id) => {
+    // Set the user to edit
+    setEditUserId(id);
+    const userToEdit = users.find((user) => user.id === id);
+    setNewUser(userToEdit);
+  };
+
+  const handleSaveEdit = () => {
+    // Update the edited user in the mock API
+    axios.put(`${API_URL}/${editUserId}`, newUser)
+      .then(() => {
+        const updatedUsers = users.map((user) => {
+          if (user.id === editUserId) {
+            return newUser;
+          }
+          return user;
+        });
+        setUsers(updatedUsers);
+        setEditUserId(null);
+        setNewUser(initialNewUser);
+      })
+      .catch((error) => {
+        console.error('Error updating user:', error);
+      });
+  };
+
+  const handleDeleteUser = (id) => {
+    // Delete a user from the mock API
+    axios.delete(`${API_URL}/${id}`)
+      .then(() => {
+        setUsers(users.filter((user) => user.id !== id));
+      })
+      .catch((error) => {
+        console.error('Error deleting user:', error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>User Management</h1>
+
+      <h2>Add Your Details</h2>
+      <h4>Scroll Down For Our Stored Data</h4>
+      <div className='user-box'>
+      <label>Name:</label>
+        <input
+          type="text"
+          placeholder="Name"
+          value={newUser.name}
+          onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+        />
+        <label>Email:</label>
+        <input
+          type="email"
+          placeholder="Email"
+          value={newUser.email}
+          onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+        />
+        <label>Username:</label>
+        <input
+          type="text"
+          placeholder="Username"
+          value={newUser.username}
+          onChange={(e) => setNewUser({ ...newUser, username: e.target.value })}
+        />
+        <label>Street:</label>
+        <input
+          type="text"
+          placeholder="Street"
+          value={newUser.address.street}
+          onChange={(e) => setNewUser({ ...newUser, address: { ...newUser.address, street: e.target.value } })}
+        />
+        <label>Suite:</label>
+        <input
+          type="text"
+          placeholder="Suite"
+          value={newUser.address.suite}
+          onChange={(e) => setNewUser({ ...newUser, address: { ...newUser.address, suite: e.target.value } })}
+        />
+        <label>City:</label>
+        <input
+          type="text"
+          placeholder="City"
+          value={newUser.address.city}
+          onChange={(e) => setNewUser({ ...newUser, address: { ...newUser.address, city: e.target.value } })}
+        />
+        <label>Zip code</label>
+        <input
+          type="text"
+          placeholder="Zipcode"
+          value={newUser.address.zipcode}
+          onChange={(e) => setNewUser({ ...newUser, address: { ...newUser.address, zipcode: e.target.value } })}
+        />
+        <label>Latitude:</label>
+        <input
+          type="text"
+          placeholder="Latitude"
+          value={newUser.address.geo.lat}
+          onChange={(e) => setNewUser({ ...newUser, address: { ...newUser.address, geo: { ...newUser.address.geo, lat: e.target.value } } })}
+        />
+        <label>Longitude</label>
+        <input
+          type="text"
+          placeholder="Longitude"
+          value={newUser.address.geo.lng}
+          onChange={(e) => setNewUser({ ...newUser, address: { ...newUser.address, geo: { ...newUser.address.geo, lng: e.target.value } } })}
+        />
+        {editUserId ? (
+          <button onClick={handleSaveEdit}>Save Edit</button>
+        ) : (
+          <button onClick={handleAddUser}>Add</button>
+        )}
+      </div>
+
+      <h2>User List</h2>
+      <ul>
+        {users.map((user) => (
+          <li className='user-box' key={user.id}>
+             <strong>Name:</strong> {user.name}<br />
+            <strong>Email:</strong> {user.email}<br />
+            <strong>Username:</strong> {user.username}<br />
+            <strong>Address:</strong> {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}<br />
+            <strong>Geo:</strong> Latitude: {user.address.geo.lat}, Longitude: {user.address.geo.lng}
+            <button onClick={() => handleEditUser(user.id)}>Edit</button>
+            <button id='del-btn' onClick={() => handleDeleteUser(user.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
