ccc706ae4cb84b9942d0ef665b8f2094a15b9007
SATHAPPAN <135624204+ZAKHAMENT@users.noreply.github.com>
2023-09-05 14:29:12 +0530

    Update store.jsx

--- Day32 task/src/store.jsx
+++ Day32 task/src/store.jsx
@@ -1,12 +1,9 @@
-// store.js
-
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 
 const store = configureStore({
   reducer: {
-    cart: cartReducer, // Add your reducer here
-    // You can add more reducers if needed
+    cart: cartReducer
   },
 });
 
