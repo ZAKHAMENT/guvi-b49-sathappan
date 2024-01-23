dde5548ec53f1896bf61bd7656d94d24b0a0d513
SATHAPPAN <135624204+ZAKHAMENT@users.noreply.github.com>
2024-01-23 18:46:02 +0530

    Update script.js

--- day16 task/script.js
+++ day16 task/script.js
@@ -1,65 +1,37 @@
-function independenceCount(callback) {
-    console.log("Let's Begin The Countdown For Independence Day");
-
-    function count() {
-        setTimeout(() => {
-            console.log("10");
-            callback();
+document.addEventListener("DOMContentLoaded", function () {
+    // Countdown function using callback hell
+    function countdown(number, callback) {
+      setTimeout(function () {
+        document.getElementById('countdown').innerText = number;
+  
+        // Nested callback for the next countdown
+        setTimeout(function () {
+          document.getElementById('countdown').innerText = '';
+          callback();
         }, 1000);
-
-        setTimeout(() => {
-            console.log("9");
-            callback();
-        }, 2000);
-
-        setTimeout(() => {
-            console.log("8");
-            callback();
-        }, 3000);
-
-        setTimeout(() => {
-            console.log("7");
-            callback();
-        }, 4000);
-
-        setTimeout(() => {
-            console.log("6");
-            callback();
-        }, 5000);
-
-        setTimeout(() => {
-            console.log("5");
-            callback();
-        }, 6000);
-
-        setTimeout(() => {
-            console.log("4");
-            callback();
-        }, 7000);
-
-        setTimeout(() => {
-            console.log("3");
-            callback();
-        }, 8000);
-
-        setTimeout(() => {
-            console.log("2");
-            callback();
-        }, 9000);
-
-        setTimeout(() => {
-            console.log("1");
-            callback();
-        }, 10000);
-
-        setTimeout(() => {
-            console.log("Happy Independence Day !!!");
-            callback();
-        }, 11000);
+      }, 1000);
     }
-
-    count(callback);
-}
-
-independenceCount(function () {
-});
+  
+    countdown(10, function () {
+      countdown(9, function () {
+        countdown(8, function () {
+          countdown(7, function () {
+            countdown(6, function () {
+              countdown(5, function () {
+                countdown(4, function () {
+                  countdown(3, function () {
+                    countdown(2, function () {
+                      countdown(1, function () {
+                        document.getElementById('message').innerText = 'Happy Independence Day!';
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+  
