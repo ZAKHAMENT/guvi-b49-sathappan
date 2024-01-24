9754fceb7615a962a92ceeae8f17cfb3112869ab
SATHAPPAN <135624204+ZAKHAMENT@users.noreply.github.com>
2024-01-24 12:31:13 +0530

    Update main.jsx

--- day23 task/src/main.jsx
+++ day23 task/src/main.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
-import ShoppingCart from './ShoppingCart.jsx'
-
+import ShoppingCart from './ShoppingCart'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
     <App />
     <ShoppingCart></ShoppingCart>
   </React.StrictMode>,
 )
+
