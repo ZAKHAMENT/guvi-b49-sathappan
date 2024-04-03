5109c7dbfdcd9a7950deef9d2720620db6bd60c2
SATHAPPAN <135624204+ZAKHAMENT@users.noreply.github.com>
2024-04-03 13:08:46 +0530

    Update server.js

--- Node-task-5.1/Back-End/server.js
+++ Node-task-5.1/Back-End/server.js
@@ -103,7 +103,7 @@ const emailTransporter = nodemailer.createTransport({
 
 // Send an email with the token link
 async function sendEmail(email, token) {
-  const resetLink = `https://660d01c77697269c4d3c9345--effulgent-panda-0eb781.netlify.app//reset-password/${token}`; 
+  const resetLink = `https://660d01c77697269c4d3c9345--effulgent-panda-0eb781.netlify.app/reset-password/${token}`; 
   const mailOptions = {
     from: 'kdsomewhatelse@gmail.com',
     to: email,
