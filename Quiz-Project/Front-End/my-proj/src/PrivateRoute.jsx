bc1c830fce239dedc21839d5d91b38f2262c52c9
SATHAPPAN <135624204+ZAKHAMENT@users.noreply.github.com>
2024-11-08 20:37:53 +0530

    Delete Quiz-Project directory

--- Quiz-Project/Front-End/my-proj/src/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('$FSA_auth_token');
-
-  return token ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
