b55e33273819609d643e964c5041bf4065e17c0a
SATHAPPAN <135624204+ZAKHAMENT@users.noreply.github.com>
2024-11-27 13:37:38 +0530

    Add files via upload

---
+++ Quiz-Project/Back-End/server.js
@@ -0,0 +1,341 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const UserModel = require('./models/User');
+const TemporaryUserModel = require('./models/TemporaryUser');
+const { OAuth2Client } = require('google-auth-library');
+//useNewUrlParser
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
+app.use(
+  session({
+    secret: "123456789",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 60000 * 60,
+      secure: true, // Set this if using HTTPS
+      httpOnly: true,
+
+    },
+    store: MongoStore.create({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day in seconds
+      connectTimeoutMS: 30000, // 30 seconds
+      mongoUrl: "mongodb+srv://sathappanramesh288:Guvi123...@cluster0.bsgotks.mongodb.net/Main_Project_1?retryWrites=true&w=majority",
+    }),
+  })  
+);
+
+const PORT = 3000;
+const DB_URL = "mongodb+srv://sathappanramesh288:Guvi123...@cluster0.bsgotks.mongodb.net/Main_Project_1?retryWrites=true&w=majority";
+
+mongoose.connect(DB_URL, {
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => {
+  console.error("Could not connect to MongoDB", err);
+  process.exit(1);
+});
+
+  // Middleware to check if the user is authenticated
+  const authenticateUser = (req, res, next) => {
+    const $FSA_auth_token = req.headers.authorization.split(" ")[1];
+    //console.log("$FSA_auth_token", $FSA_auth_token);
+    
+    if (!$FSA_auth_token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const decodedToken = jwt.verify(
+      $FSA_auth_token,
+      "123456789"
+  );
+  console.log("decided token" ,decodedToken);
+// 66c0bdb97dab30937ce9a4ed
+ //66c0bdb97dab30937ce9a4ed
+  req.user = {
+    username: decodedToken.username,
+    userId: decodedToken.userId, 
+    email: decodedToken.email,
+  }
+    console.log("Authenticated user:", req.user);
+    next();
+  };
+
+app.post('/register', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    const existingUserByUsername = await UserModel.findOne({ username });
+    const existingUserByEmail = await UserModel.findOne({ email });
+
+    if (existingUserByUsername) {
+      return res.status(400).json({ error: 'Username already exists.' });
+    }
+    if (existingUserByEmail) {
+      return res.status(400).json({ error: 'Email is already registered.' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const token = generateVerificationPin();
+
+    // Save user details in temporary collection
+    const temporaryUser = new TemporaryUserModel({
+      username,
+      email,
+      password: hashedPassword,
+      fourDigitCode: token
+    });
+    await temporaryUser.save();
+
+    // Send verification email
+    sendEmail(email, token);
+    res.status(201).json({ message: 'Verification code sent. Please check your email.', email });
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).json({ error: 'An error occurred while registering the user.' });
+  }
+});
+
+app.post('/google-register', async (req, res) => {
+  const { googleCredential } = req.body;
+
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: googleCredential,
+      audience: GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    const { sub, email, name } = payload;
+
+    let user = await UserModel.findOne({ email });
+
+    if (!user) {
+      user = new UserModel({
+        googleId: sub,
+        username: name,
+        email,
+        deviceRegistered: true,
+      });
+      await user.save();
+      return res.status(200).json({ message: 'Google login successful', $FSA_auth_token: generateToken(user) });
+    } else {
+      if (user.googleId && user.googleId === sub) {
+        return res.status(200).json({ message: 'Google login successful', $FSA_auth_token: generateToken(user) });
+      } else {
+        return res.status(400).json({ error: 'Email is already registered with a different method.' });
+      }
+    }
+  } catch (error) {
+    console.error('Error during Google login:', error);
+    res.status(500).json({ error: 'Google login failed. Please try again.' });
+  }
+});
+
+const GOOGLE_CLIENT_ID = '383869457570-gupq720fk2s1unh8bn5vag2ie58533bv.apps.googleusercontent.com';
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+
+app.post('/google-login', async (req, res) => {
+  const { googleCredential } = req.body;
+
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: googleCredential,
+      audience: GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    const { sub, email } = payload;
+
+    let user = await UserModel.findOne({ email });
+
+    if (user) {      
+      // User exists, proceed with login
+      if (user.googleId && user.googleId === sub) {
+        return res.status(400).json({ error: 'Email is already registered with a different method.' });
+      } else {        
+        return res.status(200).json({ message: 'Google login successful', $FSA_auth_token: generateToken(user) });
+      }
+    } else {
+      return res.status(400).json({ error: 'User does not exist. Please sign up first.' });
+    }
+  } catch (error) {
+    console.error('Error during Google login:', error);
+    res.status(500).json({ error: 'Google login failed. Please try again.' });
+  }
+});
+
+// Login
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password.' });
+    }
+    
+    req.user = user;
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      const $FSA_auth_token = generateToken(user);
+      res.status(200).json({ message: 'Login successful', $FSA_auth_token });
+    } else {
+      res.status(404).json({ error: 'Invalid email or password.' });
+    }
+  } catch (error) {
+    console.error('Error logging in:', error);
+    res.status(500).json({ error: 'An error occurred while logging in.' });
+  }
+});
+
+app.post('/verify-code', async (req, res) => {
+  const { email, code } = req.body;
+
+  try {
+    const temporaryUser = await TemporaryUserModel.findOne({ email, fourDigitCode: code });
+
+    if (temporaryUser) {
+      const newUser = new UserModel({
+        username: temporaryUser.username,
+        email: temporaryUser.email,
+        password: temporaryUser.password,
+        registered: true,
+        deviceRegistered: true  // Set deviceRegistered to true
+      });
+
+      await newUser.save();
+      await TemporaryUserModel.deleteOne({ email, fourDigitCode: code });
+
+      res.status(200).json({ success: true, message: 'Verification successful!' });
+    } else {
+      res.status(400).json({ success: false, message: 'Invalid verification code.' });
+    }
+  } catch (error) {
+    console.error('Error verifying code:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.get('/myProfile', authenticateUser, (req, res) => {
+  const user = req.user;
+  console.log(user.userId);
+  res.json({ username: user.username, _id: user.userId, email: user.email });
+});
+
+app.post('/api/user-temp-progress', async (req, res) => {
+  const { userId, progress, tempTag } = req.body;
+
+  if (!userId || !progress) {
+    return res.status(400).send("Missing userId or progress data");
+  }
+
+  try {
+    const user = await UserModel.findOne({ _id: userId });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    let hashedCode = 0;
+    for (let i = 0; i < tempTag.length; i++) {
+      hashedCode += tempTag.charCodeAt(i);
+    }
+
+    // Store the progress under the hashed key
+    user.tempProgress.set(String(hashedCode), progress);
+    await user.save();
+
+    res.status(200).send("Progress saved successfully!");
+  } catch (error) {
+    console.error("Error saving progress:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+
+app.post('/api/store-time', async (req, res) => {
+  const { userId, time } = req.body;
+  
+  if (!userId || time === undefined) {
+    return res.status(400).send("Missing userId or time data");
+  }
+
+  try {
+    const user = await UserModel.findOne({ _id: userId });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    user.tempProgress.time = time;
+    await user.save();
+
+    // console.log(`Time saved for user ${userId}:`, time);
+    res.status(200).send("Time saved successfully!");
+  } catch (error) {
+    console.error("Error saving time:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+
+function generateToken(user) {
+  const token = jwt.sign({username: user.username, email: user.email, password: user.password, userId: user._id }, '123456789', {
+     expiresIn: '3hr',
+  });
+  return token;
+}
+
+const emailTransporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'kdsomewhatelse@gmail.com',
+    pass: 'drugjvpuwdfzregv',
+  },
+});
+
+function sendEmail(email, token) {
+  const mailOptions = {
+    from: 'kdsomewhatelse@gmail.com',
+    to: email,
+    subject: 'Email verification',
+    text: `To join our quiz-ko, verify your email with this 4-digit pin code: ${token}`,
+  };
+  emailTransporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
+function generateVerificationPin() {
+  return Math.floor(1000 + Math.random() * 9000);
+}
+app.post('/api/track-unload', (req, res) => {
+  const { message, timestamp } = req.body;
+  console.log(`Message: ${message}, Timestamp: ${timestamp}`);
+  res.status(200).send('Unload event tracked successfully.');
+});
+app.listen(PORT, () => {
+  console.log("Server is running on PORT", PORT);
+});
---
+++ Quiz-Project/Front-End/my-proj/src/Layout.jsx
@@ -0,0 +1,255 @@
+import { useState, useEffect } from 'react';
+import { useLocation, useNavigate, Link, Outlet, } from 'react-router-dom';
+import styles from './Home.module.css';
+
+const Layout = () => {
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+    const [coinIconDisappear, setCoinIconDisappear] = useState(true);
+    const location = useLocation();
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 768) {
+                setSidebarOpen(true); 
+                setCoinIconDisappear(false);
+            } else {
+                setSidebarOpen(true); 
+                setCoinIconDisappear(true);
+            }
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    useEffect(() => {
+        const setActiveClass = () => {
+            const path = location.pathname;
+            const page = path.substr(path.lastIndexOf('/') + 1) || 'dashboard';
+            const activeLink = `${page}-link`;
+            document.querySelectorAll(`.${styles.navItem}`).forEach(item => {
+                item.classList.toggle(styles.active, item.classList.contains(activeLink));
+            });
+        };
+
+        setActiveClass();
+    }, [location]);
+
+      // Handle page unload event
+  useEffect(() => {
+    const handleUnload = () => {
+      const data = {
+        message: 'User has left the page.',
+        timestamp: new Date().toISOString(),
+      };
+
+      const url = 'http://localhost:3000/api/track-unload';
+      navigator.sendBeacon(url, JSON.stringify(data));
+    };
+
+    window.addEventListener('unload', handleUnload);
+
+    return () => {
+      window.removeEventListener('unload', handleUnload);
+    };
+  }, []);
+
+  // Redirect based on token and registration state
+  useEffect(() => {
+    const registered = localStorage.getItem('registered');
+    const token = localStorage.getItem('$FSA_auth_token');
+
+    // Check if the user is on the root path and redirect accordingly
+    if (window.location.pathname === '/') {
+      if (!registered || !token) {
+        navigate('/login'); // Redirect to login if not registered or no token
+      }
+    }
+  }, [navigate]);
+
+    // Other Handles
+    const toggleSidebar = () => {
+        setSidebarOpen(prevState => !prevState);
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('$FSA_auth_token');
+        navigate('/login');
+    };
+
+    return (
+        <div className={styles.homeBody}>
+            <main role="main" className={`${styles.containerFluid}`} id="code-kata-page">
+                {/* Header sidebarWrapper */}
+                <header className={` ${styles.homeHeader} ${styles.row} ${styles.bgWhite}`}>
+                    <nav className={`${styles.col} ${styles.navbar} ${styles.navbarExpandMd} ${styles.navbarLight} ${styles.shadowSm} ${styles.guviNavheadFixed} ${styles.containerFluid2}`}>
+                        <button
+                            id="sidebarToggle"
+                            className={styles.navbarToggler}
+                            type="button"
+                            onClick={toggleSidebar}
+                            aria-controls="sidebar"
+                            aria-expanded={isSidebarOpen}
+                            aria-label="Toggle navigation">
+                            <i className={styles.materialIcons}>{isSidebarOpen ? 'close' : 'menu'}</i>
+                        </button>
+                        <a className={`${styles.navbarBrand} ${styles.aHome}`} href="#">
+                            <img src="/images/main-logo.jpeg" className={`${styles.lazyload} ${styles.ml3}`} alt="Site logo" width="95px" />
+                        </a>
+{/*  ---------------------------------- Coin conatiner --------------------------------------------- */}
+                  {coinIconDisappear && (
+                        <div className={`${styles.buttonNavbar} ${styles.coinContainer}`}>
+                        <div className={styles.parent}>
+                        <div className={styles.div1}>
+                                <img src="/images/coinCoinImage.png" alt="icon" className={styles.coinIcon}></img><span className={styles.coinText}>       6900</span>    
+                        </div>
+                        <div className={styles.div2}>
+                                <img width="48" height="48" src="https://img.icons8.com/emoji/48/star-emoji.png" alt="icon" className={styles.coinIcon}></img><span className={styles.coinText}>       6900</span>   
+                            </div>
+                        </div>
+                        </div>
+                  )}
+{/*------------------------------------------ User Icon ------------------------------------*/}
+                        <div className={styles.buttonNavbar}>
+                            <button
+                                className={`${styles.btn} ${styles.dropdown} ${styles.accountBoxToggler}`}
+                                type="button"
+                                data-toggle="collapse"
+                                data-target="#account-box"
+                                aria-expanded="false"
+                                aria-controls="account-box"
+                            >
+                                <label className={styles.popup}>
+                                    <input type="checkbox" />
+                                    <div className={styles.burger} tabIndex="0">
+                                        <svg width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M335 343.43H65V300.06C65 254.02 102.33 216.69 148.37 216.69H251.63C297.67 216.69 335 254.02 335 300.06V343.43Z"
+                                                stroke="#191919"
+                                                strokeWidth="12"
+                                                strokeMiterlimit="10"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                            <path
+                                                d="M200 186.57C235.899 186.57 265 157.468 265 121.57C265 85.6713 235.899 56.5698 200 56.5698C164.101 56.5698 135 85.6713 135 121.57C135 157.468 164.101 186.57 200 186.57Z"
+                                                stroke="#6A0BFF"
+                                                strokeWidth="12"
+                                                strokeMiterlimit="10"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                        </svg>
+                                    </div>
+<nav className={styles.popupWindow}>
+    <legend>Actions</legend>
+    <ul>
+      <li>
+        <Link to='/profile' style={{textDecoration: 'none'}}>
+        <button className={styles.popupBtn} >
+          <svg strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" stroke="currentColor" fill="none" viewBox="0 0 24 24" height="14" width="14" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+            <circle r="4" cy="7" cx="9"></circle>
+            <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+            <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+          </svg>
+          <span>My Profile</span> 
+        </button>
+        </Link>
+      </li>
+      <li>
+      <Link to='/change-password' style={{textDecoration: 'none'}}>
+        <button className={styles.popupBtn} >
+      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" >
+    <path fill="currentColor" d="M15 6a1.54 1.54 0 0 1-1.5-1.5a1.5 1.5 0 0 1 3 0A1.54 1.54 0 0 1 15 6zm-1.5-5A5.55 5.55 0 0 0 8 6.5a6.81 6.81 0 0 0 .7 2.8L1 17v2h4v-2h2v-2h2l3.2-3.2a5.85 5.85 0 0 0 1.3.2A5.55 5.55 0 0 0 19 6.5A5.55 5.55 0 0 0 13.5 1z"></path>
+      </svg>  
+          <span>Change Password</span>
+        </button>
+      </Link>
+      </li>
+      <li>
+        <button className={styles.popupBtn} onClick={handleLogout}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 14 14">
+    <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M9.5 10.5v2a1 1 0 0 1-1 1h-7a1 1 0 0 1-1-1v-11a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v2M6.5 7h7m-2-2l2 2l-2 2"></path>
+    </svg>
+       <span>Sign out</span>
+        </button>
+      </li>
+    </ul>
+  </nav>
+  </label>
+</button>
+</div>
+ </nav>
+
+{/*------------------------------------------------ Sidebar ---------------------------------*/}
+                    <div className={styles.sidebarWrapper}>
+                        <div className={`${styles.sidebarContainer} ${isSidebarOpen ? '' : styles.sidebarCollapse}`}>
+                            <div id="sidebar" className={`${styles.sidebarSticky} ${styles.shadowSm}`}>
+                                <ul className={`${styles.nav} ${styles.flexColumn} ${styles.mainMenu}`}> Category
+                                    <li className={`${styles.navItem} ${styles.coursesLink}`}>
+                                        <Link to="/gk/all">
+                                        <a className={`${styles.navLink} ${styles.aHome}`}>
+                                            <img src="/images/question-mark.png" alt="?" width="30px" />
+                                            <i className={styles.iconsCourses}></i>
+                                                <span className={styles.sidebarInlineText}>GK </span>
+                                        </a>
+                                        </Link>
+                                    </li>
+                                    <hr className={styles.borderSidebar} />
+                                    <li className={`${styles.navItem} ${styles.codeKataLink}`}>
+                                        <a className={`${styles.navLink} ${styles.aHome}`} href="/code-kata/">
+                                            <img src="/images/parrot.png" alt="?" width="30px" />
+                                            <i className={styles.iconsCodeKata}></i>
+                                            <span className={styles.sidebarInlineText}>Birds</span>
+                                        </a>
+                                    </li>
+                                    <hr className={styles.borderSidebar} />
+                                    <li className={`${styles.navItem} ${styles.leaderBoardLink}`}>
+                                        <a className={`${styles.navLink} ${styles.aHome}`} href="/leader-board/">
+                                            <img src="/images/lion.png" alt="?" width="30px" />
+                                            <i className={styles.iconsLeaderboard}></i>
+                                            <span className={styles.sidebarInlineText}>Animals</span>
+                                        </a>
+                                    </li>
+                                    <li className={`${styles.navItem} ${styles.rewardsLink}`}>
+                                        <a className={`${styles.navLink} ${styles.aHome}`} href="/rewards/">
+                                            <img src="/images/question-mark.png" alt="?" width="30px" />
+                                            <i className={styles.iconsRewards}></i>
+                                            <span className={styles.sidebarInlineText}>Rewards</span>
+                                        </a>
+                                    </li>
+                                    <li className={`${styles.navItem} ${styles.referralLink}`}>
+                                        <a className={`${styles.navLink} ${styles.aHome}`} href="/referral/">
+                                            <img src="/images/question-mark.png" alt="?" width="30px" />
+                                            <i className={styles.iconsReferral}></i>
+                                            <span className={styles.sidebarInlineText}>Referral</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <div className={styles.staticHeight}></div>
+                {/* /Header */}
+                <div className={styles.containerFluid}>
+{/*  -------------------------- Main contents --------------------------------------------------- */}
+                    <div className={styles.row}>
+                        <div className={`${styles.colSm12} ${styles.offsetMd1} ${styles.colMd11}`}>
+                            <div className={styles.bow}>                                
+                                <div className={`${styles.colSm12} ${styles.colMd6} ${styles.dFlex} ${styles.flexColumn} ${styles.justifyContentAround}`}>
+                                    <Outlet />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
