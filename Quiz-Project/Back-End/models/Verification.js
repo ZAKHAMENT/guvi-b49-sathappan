e7587a422322423c0c76f4f5b973bfa0f28c8f35
SATHAPPAN <135624204+ZAKHAMENT@users.noreply.github.com>
2024-12-27 20:02:15 +0530

    Delete Quiz-Project directory

--- Quiz-Project/Back-End/models/Verification.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const verifySchema = new mongoose.Schema({
-    email: String,
-    fourDigitCode: String, 
-  });
-  
-  const Verfication = mongoose.model('Verification', verifySchema);
-  module.exports = Verfication;
